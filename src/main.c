ac5c0b4662e6087fa9fce24eab1644947244ad7c
Don Browne <brownedon@comcast.net>
2014-11-16 17:02:06 -0500

    initial version

---
+++ src/main.c
@@ -0,0 +1,139 @@
+#include "pebble.h"
+
+static Window *window;
+
+static TextLayer *glucose_layer;
+static TextLayer *s_time_layer;
+
+static char glucose[16];
+
+static BitmapLayer *icon_layer;
+static GBitmap *icon_bitmap = NULL;
+
+static AppSync sync;
+static uint8_t sync_buffer[32];
+
+enum GlucoseKey {
+  GLUCOSE_KEY = 0x1,  // TUPLE_CSTRING
+};
+
+static void sync_error_callback(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "App Message Sync Error: %d", app_message_error);
+}
+
+static void sync_tuple_changed_callback(const uint32_t key, const Tuple* new_tuple, const Tuple* old_tuple, void* context) {
+  switch (key) {
+     case GLUCOSE_KEY:
+      // App Sync keeps new_tuple in sync_buffer, so we may use it directly
+      text_layer_set_text(glucose_layer, new_tuple->value->cstring);
+      break;
+  }
+}
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Create a long-lived buffer
+  static char buffer[] = "00:00";
+
+  // Write the current hours and minutes into the buffer
+  if(clock_is_24h_style() == true) {
+    //Use 2h hour format
+    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  } else {
+    //Use 12 hour format
+    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
+  }
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, buffer);
+}
+  
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+  
+
+
+static void window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  
+  //clock
+  // Create time TextLayer
+  s_time_layer = text_layer_create(GRect(0, 20, 144, 50));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+
+  // Improve the layout to be more like a watchface
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  //
+  //
+  //glucose
+  glucose_layer = text_layer_create(GRect(0, 100, 144, 68));
+  text_layer_set_background_color(glucose_layer, GColorClear);
+  text_layer_set_text_color(glucose_layer, GColorWhite);
+  text_layer_set_font(glucose_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_text_alignment(glucose_layer, GTextAlignmentCenter);
+  text_layer_set_text(glucose_layer, glucose);
+
+  Tuplet initial_values[] = {
+    TupletCString(GLUCOSE_KEY, "000"),
+  };
+  app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values, ARRAY_LENGTH(initial_values),
+                sync_tuple_changed_callback, sync_error_callback, NULL);
+
+  layer_add_child(window_layer, text_layer_get_layer(glucose_layer));
+   
+  // Make sure the time is displayed from the start
+  update_time();
+  
+}
+
+static void window_unload(Window *window) {
+  app_sync_deinit(&sync);
+
+  if (icon_bitmap) {
+    gbitmap_destroy(icon_bitmap);
+  } 
+
+  text_layer_destroy(glucose_layer);
+  text_layer_destroy(s_time_layer);
+  bitmap_layer_destroy(icon_layer);
+}
+
+static void init() {
+  window = window_create();
+  window_set_background_color(window, GColorBlack);
+  window_set_fullscreen(window, true);
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload
+  });
+
+  const int inbound_size = 64;
+  const int outbound_size = 16;
+  app_message_open(inbound_size, outbound_size);
+
+  // Register with TickTimerService
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  const bool animated = true;
+  window_stack_push(window, animated);
+}
+
+static void deinit() {
+  window_destroy(window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
